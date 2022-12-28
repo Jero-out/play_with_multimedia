a43b12eb943d1847b2e5734ceb5e3af09e0d4ca4
unknown <987068064@qq.com>
2022-12-28 16:17:30 +0800

    modified:   build.sh

--- ffmpeg/build.sh
+++ ffmpeg/build.sh
@@ -12,53 +12,57 @@ opus_pkg=opus-1.3.1.tar.gz
 ffmpeg_pkg=ffmpeg-4.4.3.tar.bz2
 
 build_nasm() {
-    tar xjf $nasm_pkg
-    cd nasm-2.14.02
-    ./autogen.sh
-    ./configure --prefix="$release_path/nasm" --bindir="$release_path/nasm/bin"
-    make &&
+	cd $current_path
+    tar xjf $nasm_pkg && \
+    cd nasm-2.14.02 && \
+    ./autogen.sh && \
+    ./configure --prefix="$release_path/nasm" --bindir="$release_path/nasm/bin" && \
+    make && \
     make install
 }
 
 build_yasm() {
-    tar xzf $nasm_pkg
-    cd yasm-1.3.0
-    ./configure --prefix="$release_path/yasm" --bindir="$release_path/yasm/bin"
-    make
+	cd $current_path
+    tar xzf $yasm_pkg && \
+    cd yasm-1.3.0 && \
+    ./configure --prefix="$release_path/yasm" --bindir="$release_path/yasm/bin" && \
+    make && \
     make install
 }
 
 build_x264() {
-    tar xjf $x264_pkg
-    cd x264-snapshot-20191217-2245
-    PATH="$HOME/bin:$PATH" PKG_CONFIG_PATH="$HOME/ffmpeg_build/lib/pkgconfig" 
-    ./configure --prefix="$release_path/x264" --bindir="$release_path/x264/bin" --enable-static --enable-pic
-    make
+	cd $current_path
+    tar xjf $x264_pkg && \
+    cd x264-snapshot-20191217-2245 && \
+    PATH="$release_path/nasm/bin:$release_path/yasm/bin:$PATH" && \
+    ./configure --prefix="$release_path/x264" --bindir="$release_path/x264/bin" --enable-static --enable-pic && \
+    make && \
     make install
 }
 
 build_x265() {
-    tar xzf $x265_pkg
-    cd x265_3.2
-    PATH="$HOME/bin:$PATH" 
-    cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$release_path/x265" -DENABLE_SHARED=off ./source
-    make
+	cd $current_path
+    tar xzf $x265_pkg && \
+    cd x265_3.2 && \
+    cmake -G "Unix Makefiles" -DCMAKE_INSTALL_PREFIX="$release_path/x265" -DENABLE_SHARED=off ./source && \
+    make && \
     make install
 }
 
 build_aac() {
-    tar xzf $aac_pkg
-    cd fdk-aac-2.0.2
-    autoreconf -fiv
-    ./configure --prefix="$release_path/aac" --disable-shared
-    make
+	cd $current_path
+    tar xzf $aac_pkg && \
+    cd fdk-aac-2.0.2 && \
+    autoreconf -fiv && \
+    ./configure --prefix="$release_path/fdk-aac" --disable-shared && \
+    make && \
     make install
 }
 
 build_opus() {
+	cd $current_path
     tar xzf $opus_pkg && \
     cd opus-1.3.1 && \
-    # ./autogen.sh && \
     ./configure --prefix="$release_path/opus" --disable-shared && \
     make && \
     make install
@@ -72,17 +76,22 @@ build_aac
 build_opus
 
 build_ffmpeg() {
-    tar xjf $ffmpeg_pkg && \
-    cd ffmpeg-4.4.3 && \
-    ./configure \
+	cd $current_path
+    #tar xjf $ffmpeg_pkg
+    cd ffmpeg-4.4.3
+	echo $PATH
+    PATH="$release_path/nasm/bin:$release_path/yasm/bin:$PATH"
+	PKG_CONFIG_PATH="$release_path/opus/lib/pkgconfig:$release_path/x265/lib/pkgconfig"
+	./configure \
     --prefix="$release_path/ffmpeg" \
     --pkg-config-flags="--static" \
-    --extra-cflags="-I$release_path/*/include" \
-    --extra-ldflags="-L$release_path/*/lib" \
-    --extra-libs="-lpthread -lm" \
+    --extra-cflags="-I$release_path/fdk-aac/include -I$release_path/opus/include" \
+    --extra-cflags="-I$release_path/x264/include -I$release_path/x265/include" \
+    --extra-ldflags="-L$release_path/fdk-aac/lib -L$release_path/opus/lib" \
+    --extra-ldflags="-L$release_path/x264/lib -L$release_path/x265/lib" \
+    --extra-libs="-lpthread -lm -ldl" \
     --bindir="$release_path/ffmpeg/bin" \
     --enable-gpl \
-    --enable-libass \
     --enable-libfdk-aac \
     --enable-libfreetype \
     --enable-libopus \
